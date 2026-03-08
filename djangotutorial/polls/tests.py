d65162ab25bbb66807a4d9aa8d1d020f1f8570d4
Forzana Rime <fkr206@nyu.edu>
2026-03-08 17:46:40 -0400

    tests pass on local

--- djangotutorial/polls/tests.py
+++ djangotutorial/polls/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Question
 
-# Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def test_str_returns_question_text(self):
+        question = Question(question_text="Hello?")
+        self.assertEqual(str(question), "Hello?")
